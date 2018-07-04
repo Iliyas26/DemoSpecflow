9e8509e5e5ff87574dda235a48dab2dd85c7e48c
Iliyas26 <40816746+Iliyas26@users.noreply.github.com>
2018-07-04 12:35:25 +0530

    Commenting steps

--- foo.specs/FirstFeatureSteps.cs
+++ foo.specs/FirstFeatureSteps.cs
@@ -9,20 +9,19 @@ namespace foo.specs
         [Given]
         public void Given_I_have_entered_P0_into_the_calculator(int p0)
         {
-            //if()
-            ScenarioContext.Current.Add("number1", p0);
+            
         }
 
         [Given(@"I again entered (.*) into the calculator")]
         public void GivenIAgainEnteredIntoTheCalculator(int p0)
         {
-            ScenarioContext.Current.Add("number2", p0);
+            
         }
 
         [When]
         public void When_I_press_add()
         {
-            var number = Convert.ToInt64(ScenarioContext.Current["number1"]) + Convert.ToInt64(ScenarioContext.Current["number2"]);
+            //var number = Convert.ToInt64(ScenarioContext.Current["number1"]) + Convert.ToInt64(ScenarioContext.Current["number2"]);
         }
 
         [When]
@@ -34,7 +33,7 @@ namespace foo.specs
         [Then]
         public void Then_the_result_should_be_P0_on_the_screen(int p0)
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
     }
 }
