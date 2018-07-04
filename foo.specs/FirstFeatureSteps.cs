17df50b37252a510eb2b88eb6cf132b439398fd6
Iliyas26 <40816746+Iliyas26@users.noreply.github.com>
2018-07-04 10:43:05 +0530

    Initial commit

---
+++ foo.specs/FirstFeatureSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace foo.specs
+{
+    [Binding]
+    public class FirstFeatureSteps
+    {
+        [Given]
+        public void Given_I_have_entered_P0_into_the_calculator(int p0)
+        {
+            //if()
+            ScenarioContext.Current.Add("number1", p0);
+        }
+        
+        [When]
+        public void When_I_press_add()
+        {
+            var number = ScenarioContext.Current["number1"];
+        }
+
+        [When]
+        public void When_I_press_Substract()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then]
+        public void Then_the_result_should_be_P0_on_the_screen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
